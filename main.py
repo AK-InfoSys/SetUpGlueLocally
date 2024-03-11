1d7c7b0db172385fbf5b500e8493b4d082e308d1
JAIN <70506747+AK-InfoSys@users.noreply.github.com>
2024-03-11 11:44:08 +0530

    Steps to set up aws glue locally with mongoDB connector

--- main.py
+++ main.py
@@ -10,32 +10,25 @@ spark = SparkSession. \
     config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1"). \
     config("spark.sql.mapKeyDedupPolicy", "LAST_WIN"). \
     config("spark.mongodb.read.connection.uri",
-           "mongodb+srv://mumbaidevdsp:4z6XaZlfWxvRXZ9g@npsdevdb.iqqr8.mongodb.net/?tls=true"). \
+           "mongodb://localhost:27017/"). \
     getOrCreate()
 
-mongo_uri = "mongodb+srv://mumbaidevdsp:4z6XaZlfWxvRXZ9g@npsdevdb.iqqr8.mongodb.net/npsdevdb."
+mongo_uri = "mongodb://localhost:27017/dbNAME."
 
-user_df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri", mongo_uri + "users").load()
-survey_df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri", mongo_uri + "surveys").load()
-user_tracking_map_df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri",
-                                                                                       mongo_uri + "userTrackingMap").load()
-survey_response_df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri",
-                                                                                     mongo_uri + "surveyResponse").load()
+df1 = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri", mongo_uri + "collectionNAME").load()
 
-user_df.createOrReplaceTempView("users_df")
-survey_df.createOrReplaceTempView("survey_df")
-user_tracking_map_df.createOrReplaceTempView("user_tracking_map_df")
-survey_response_df.createOrReplaceTempView("survey_response_df")
+df1.createOrReplaceTempView("users_df")
+
 
 # survey_df.show()
 # user_tracking_map_df.show()
 # survey_response_df.show()
 
 
-npsSchema = StructType([
+Schema = StructType([
     StructField("_id", StringType()),
-    StructField("uhid", StringType()),
-    StructField("trackingIdResponseMap", MapType(
+    StructField("data", StringType()),
+    StructField("map", MapType(
         StringType(),
         StructType([
             StructField("$id", StringType())
@@ -43,29 +36,16 @@ npsSchema = StructType([
     )),
 ])
 
-user_df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri", mongo_uri + "users") \
-    .schema(npsSchema).load()
 
 print("user_df")
-user_df.show(5, False)
+df1.show(5, False)
 
-for row in survey_df.collect():
+for row in df1.collect():
     surveyId = row["_id"]
     try:
 
-        exploded_df = user_df.select("uhid", explode(map_values("trackingIdResponseMap")).alias("trackingIdResponse"))
+        exploded_df = df1.select("data", explode(map_values("map")).alias("mapResponse"))
 
-        # exploded_df = user_df.select(
-        #     "uhid",
-        #     explode("trackingIdResponseMap").alias("trackingIdResponse")
-        # ).filter(col("trackingIdResponse.surveyId") == surveyId)
-
-        # # Perform the join operation
-        # joined_df = exploded_df.join(
-        #     user_tracking_map_df,
-        #     exploded_df["trackingIdResponse"] == user_tracking_map_df["_id"],
-        #     "inner"
-        # )
         print("exploded_df")
         exploded_df.show(5, False)
         break
